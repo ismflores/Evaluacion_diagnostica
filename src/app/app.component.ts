c22827ad76ec4eaf890dffeeb4fbabad556c31e4
ismflores <156021656+ismflores@users.noreply.github.com>
2024-01-11 01:44:52 -0600

    Add files via upload

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,3 +1,4 @@
+// importar modulos de formularios para poder manipular correctamente se importa en app.component
 import { Component } from '@angular/core';
 
 @Component({
@@ -6,5 +7,21 @@ import { Component } from '@angular/core';
   styleUrls: ['./app.component.css']
 })
 export class AppComponent {
-  title = 'calculadora';
+  cuadroOperaciones: string = '';
+
+  anadirNumero(numero: string): void {
+    this.cuadroOperaciones += numero;
+  }
+
+  resultado(): void {
+    try {
+      this.cuadroOperaciones = eval(this.cuadroOperaciones); //eval evalua el operador para generar un resultado
+    } catch (error) {
+      this.cuadroOperaciones = 'Error';
+    }
+  }
+
+  borrar(): void {
+    this.cuadroOperaciones = '';
+  }
 }
